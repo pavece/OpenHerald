5360b94730e2a788eceb53ddee2f7a55c294aef3
Pavece <55211797+pavece@users.noreply.github.com>
2024-07-17 18:07:34 +0200

    Add date formatting to featured article card

--- src/components/main-page/featured-article-card.tsx
+++ src/components/main-page/featured-article-card.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { ImageContainer } from '../ui/image-container';
 import Link from 'next/link';
+import { format, parseISO } from 'date-fns';
+import { ImageContainer } from '../ui/image-container';
 import { Badge } from '../ui/badge';
 import { PiCalendar, PiClock, PiUser } from 'react-icons/pi';
 
@@ -18,24 +18,24 @@ export const FeaturedArticleCard = ({ author, date, readingTime, title, thumbnai
 	return (
 		<article className='min-w-full'>
 			<h4 className='text-xl text-zinc-500 tracking-wide mb-2'>Featured Article</h4>
-			<Link href={`/posts/${slug}`}>
+			<Link href={`/article/${slug}`}>
 				<ImageContainer url={thumbnail} alt={thumbnailAlt} className='h-[350px] w-full' />
 			</Link>
-			<div className='mt-2 flex flex-row gap-2'>
-				<Link href={`/authors/${author}`}>
+			<div className='mt-2 flex flex-row gap-2 flex-wrap'>
+				<Link href={`/author/${author}`}>
 					<Badge variant='outline' className='text-sm font-normal py-1 text-zinc-700'>
 						<PiUser size={20} className='mr-2' /> {author}
 					</Badge>
 				</Link>
 				<Badge variant='outline' className='text-sm font-normal py-1 text-zinc-700'>
-					<PiCalendar size={20} className='mr-2' /> {date}
+					<PiCalendar size={20} className='mr-2' /> {format(parseISO(date), 'dd, LLLL')}
 				</Badge>
 				<Badge variant='outline' className='text-sm font-normal py-1 text-zinc-700'>
 					<PiClock size={20} className='mr-2' /> {readingTime} min read
 				</Badge>
 			</div>
 			<div>
-				<Link href={''}>
+				<Link href={`/article/${slug}`}>
 					<h1 className='mt-3 text-2xl font-semibold hover:text-zinc-600 transition-colors duration-150'>{title}</h1>
 				</Link>
 			</div>
